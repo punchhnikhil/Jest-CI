004409d00f1d0dfd50be7a770a5aa7cfc53f8a60
Nikhil Singh Gangwar <nikhil.singh@punchh.com>
2022-09-30 19:12:34 +0530

    Initialising test app

---
+++ JestCI/App.test.js
@@ -0,0 +1,10 @@
+import {render, screen, fireEvent} from '@testing-library/react-native';
+import App from './App';
+
+describe('form submits two answers', function () {
+  const allQuestions = ['q1', 'q2'];
+  const mockFn = jest.fn();
+  it('renders Card Label', () => {
+    render(<App questions={allQuestions} onSubmit={mockFn} />);
+  });
+});
